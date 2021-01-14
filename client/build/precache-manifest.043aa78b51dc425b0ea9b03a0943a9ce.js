aacc329826576c7bc085ef90e6e023caecd94b2f
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-01-14 08:40:56 -0800

    Uploading /build

---
+++ client/build/precache-manifest.043aa78b51dc425b0ea9b03a0943a9ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f1cb46795be0bf5cbbd054ddf12262",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17202321c87de4c78ea4",
+    "url": "/static/css/main.bfc9da05.chunk.css"
+  },
+  {
+    "revision": "54ec82a9d58f9238d26b",
+    "url": "/static/js/2.5ae41c6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5ae41c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17202321c87de4c78ea4",
+    "url": "/static/js/main.5f7d2d9f.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
