bcc37ca603b7210f0143d09ee742075d6daab4de
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-01-14 14:57:43 -0800

    Fixed bug in index.js

--- server/index.js
+++ server/index.js
@@ -77,10 +77,13 @@ app.use(function (err, req, res, next) {
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
+  // app.use(express.static("/build"));
+  app.use(express.static("./client/build"));
+  // app.use(express.static("client/build"));
 
   // index.html for all page routes    html or routing and naviagtion
   app.get("*", (req, res) => {
+    // res.sendFile(path.resolve(__dirname, "./build", "build", "index.html"));
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
