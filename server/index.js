927d5f42c44b4f67d13da9b6c01d433bd273b57a
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-01-14 17:43:41 -0800

    .

--- server/index.js
+++ server/index.js
@@ -130,22 +130,30 @@ app.use("/post", postRouter);
 //   });
 // }
 
-// Serve static assets if in production
+// // Serve static assets if in production
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   // All the javascript and css files will be read and served from this folder
+//   // app.use(express.static("/build"));
+//   // app.use(express.static("../client/build"));
+//   //  app.use(express.static("./client/build"));
+
+//   app.use(express.static("../client/build"));
+
+//   // index.html for all page routes    html or routing and naviagtion
+//   app.get("*", (req, res) => {
+//     // res.sendFile(path.resolve(__dirname, "./build", "build", "index.html"));
+
+//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+//     // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  // All the javascript and css files will be read and served from this folder
-  // app.use(express.static("/build"));
-  // app.use(express.static("../client/build"));
-  //  app.use(express.static("./client/build"));
+  app.use(express.static("client/build"));
 
-  app.use(express.static("../client/build"));
-
-  // index.html for all page routes    html or routing and naviagtion
   app.get("*", (req, res) => {
-    // res.sendFile(path.resolve(__dirname, "./build", "build", "index.html"));
-
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-    // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); // relative path
   });
 }
 
