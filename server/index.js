ad79679fb8c85d1533e38389e26a8f18f38978bf
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-01-14 16:20:03 -0800

    try

--- server/index.js
+++ server/index.js
@@ -47,6 +47,25 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  // All the javascript and css files will be read and served from this folder
+  // app.use(express.static("/build"));
+  // app.use(express.static("../client/build"));
+  //  app.use(express.static("./client/build"));
+  
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes    html or routing and naviagtion
+  app.get("*", (req, res) => {
+    // res.sendFile(path.resolve(__dirname, "./build", "build", "index.html"));
+
+    // res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //Routers
 var indexRouter = require("./routes/index");
 var userRouter = require("./routes/user");
@@ -73,24 +92,24 @@ app.use(function (err, req, res, next) {
   // res.render("error");
 });
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  // All the javascript and css files will be read and served from this folder
-  // app.use(express.static("/build"));
-  // app.use(express.static("../client/build"));
-  //  app.use(express.static("./client/build"));
-  
-  app.use(express.static("client/build"));
+// // Serve static assets if in production
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   // All the javascript and css files will be read and served from this folder
+//   // app.use(express.static("/build"));
+//   // app.use(express.static("../client/build"));
+//   //  app.use(express.static("./client/build"));
 
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    // res.sendFile(path.resolve(__dirname, "./build", "build", "index.html"));
+//   app.use(express.static("client/build"));
 
-    // res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   // index.html for all page routes    html or routing and naviagtion
+//   app.get("*", (req, res) => {
+//     // res.sendFile(path.resolve(__dirname, "./build", "build", "index.html"));
+
+//     // res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const port = process.env.PORT || 5000;
 
