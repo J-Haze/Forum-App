b76a967aff9e44da0368389d6c5a2e974f894324
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-01-14 17:27:07 -0800

    .

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use(passport.session());
 
 // app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
 
@@ -95,21 +95,21 @@ app.use("/post", postRouter);
 //   });
 // }
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  // res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   // res.render("error");
+// });
 
 // // Serve static assets if in production
 // if (process.env.NODE_ENV === "production") {
